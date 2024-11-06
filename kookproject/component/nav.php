b868e9696127447e95a8b56d584799d26f64be38
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-11-06 10:03:07 +0100

    fixed nav

--- kookproject/component/nav.php
+++ kookproject/component/nav.php
@@ -11,10 +11,10 @@
 			<b>MediTurn</b>
 		</div>
 		<ul class="menu">
-			<li><a href="/">Home</a></li>
-            <li><a href="/medicijnen">Medicijnen</a></li>
-			<li><a href="/database">Database</a></li>
-			<li><a href="/about">Over ons</a></li>
-			<li><a href="/contact">Contact</a></li>
+			<li><a href="/kookproject">Home</a></li>
+            <li><a href="./medicijnen">Medicijnen</a></li>
+			<li><a href="./database">Database</a></li>
+			<li><a href="./about">Over ons</a></li>
+			<li><a href="./contact">Contact</a></li>
 		</ul>
 	</header>	<?php } ?>
