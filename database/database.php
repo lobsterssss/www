2483fe0d7c2d4940fec46029571fccdefc636e25
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-11-08 10:09:03 +0100

    makes login more actual
    changed the register to take more veriables
    and made words dutch to be compatible with the database

--- database/database.php
+++ database/database.php
@@ -10,13 +10,14 @@ class Database
 
     function __construct()
     {
-        // $hostname = '195.168.0.3:3306';
-        $username = 'root';
-        $password = '';
-        $dbname = 'test_kookproject';
-        $this->binary = hex2bin('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
-        $this->conn = new PDO("mysql:dbname=$dbname", $username, $password);
-    }
+    $hostname = '127.0.0.1';
+    // $hostname = '195.168.0.3:3306';
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'mediturn';
+    $this->binary = hex2bin('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
+    $this->conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+   }
 
     function close()
     {
@@ -26,11 +27,11 @@ class Database
     function get_Login($email, $password)
     {
         //check validation AND users.verified = 1
-        $result = $this->conn->query("SELECT users.userName, users.id, users.password, user_settings.theme FROM users INNER JOIN user_settings ON users.id = user_settings.user WHERE  users.email = '$email'")->fetch();
-        if (!$result)
-            return;
-        if ($this->decryped($result["password"]) == $password)
-            return $result;
+        $result = $this->conn->query("SELECT users.naam, users.klant_id, users.wachtwoord FROM users WHERE users.email = '$email'")->fetch();
+        if(!$result)
+        return;
+        if($this->decryped($result["wachtwoord"]) == $password)
+        return $result;
     }
 
     function get_all_medicine()
@@ -47,16 +48,16 @@ class Database
         return $result;
     }
 
-    function set_user($email, $password, $username)
+    function set_user($email, $password, $name, $lastname, $telefoon)
     {
 
         $temp = $this->conn->query("SELECT users.email FROM users WHERE users.email = '$email'")->fetchAll();
         if (!isset($temp[0])) {
 
             $password = $this->encryped($password);
-            $result = $this->conn->query("INSERT INTO users (email, userName, password) VALUES ('$email','$username','$password')");
-            if ($result)
-                $this->conn->query("INSERT INTO user_settings (user_settings.user) SELECT users.id FROM users WHERE users.email = '$email'");
+            $result = $this->conn->query("INSERT INTO users (email, naam, achternaam, wachtwoord, telefoon) VALUES ('$email','$name','$lastname','$password','$telefoon')");
+            // if($result)
+            //     $this->conn->query("INSERT INTO user_settings (user_settings.user) SELECT users.id FROM users WHERE users.email = '$email'");
             return $result;
         }
     }
