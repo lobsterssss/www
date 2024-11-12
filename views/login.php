bdd19e06594cae78629c68b9568bd207cffe04fe
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-11-12 09:34:08 +0100

    made the lofin/register css
    has a few inperfections for the fact the login is not centerd

--- views/login.php
+++ views/login.php
@@ -5,15 +5,18 @@
     <body>
         <?= navigation(); ?>
         <main>
-            <div>
-                <h1>Login</h1>
+            <div class="form-container">
+                <h2>Login</h2>
                 <form hx-post="./login" hx-target="#target">
                     <p id="target"></p>
                     <label for="userEmail">Email</label>
                     <input name="userEmail" id="userEmail" type="email" placeholder="Email">
                     <label for="userPass">Password</label>
                     <input name="userPass" id="userPass" type="password" placeholder="Password">
-                    <button type="submit">Login</button>
+                    <div>
+                        <a href="./register">nog geen acount, registreer</a>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         </main>
