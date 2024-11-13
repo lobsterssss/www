e79620a0f9bfe87354c4c1443eae0b3ca2021a46
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-11-13 10:18:11 +0100

    reworked artikal size a bit
    made about page work requires testing for mobile and smaller screen

--- views/about.php
+++ views/about.php
@@ -12,37 +12,55 @@ head();
     
     ?>
     <main>
-        <div class="about-section">
-            <h2>Over MediTurn</h2>
-            <p class="AS">Welkom bij <strong>MediTurn</strong>, uw betrouwbare platform voor informatie over onze 
-                innovatieve pillenautomaat. Deze machine is speciaal ontworpen om ouderen te helpen hun medicatie 
-                op tijd in te nemen, met een ingebouwde timer en alarm. Wij begrijpen hoe belangrijk het is dat medicatie 
-                niet wordt vergeten, en met MediTurn streven we ernaar om dit proces eenvoudiger en veiliger te maken.</p>
-        
-            <p class="AS">Onze website biedt uitgebreide informatie over de werking van onze pillenautomaat en hoe 
-                deze ouderen ondersteunt in hun dagelijkse medicatiebeheer. De automaat zorgt ervoor dat medicatie op het juiste tijdstip wordt afgegeven, zodat gebruikers hun gezondheid beter kunnen beheren.</p>
-        
-            <h3>Wat Wij Bieden</h3>
-            <ul class="info-list">
-                <li><strong2>Automatische medicijnafgifte</strong2>
+        <article>
+            <div>
+                <img src="" alt="insert img from team">
+                <div>
+                <h2>Over MediTurn</h2>
+
+                    <p>Welkom bij <strong>MediTurn</strong>, uw betrouwbare platform voor informatie over onze 
+                        innovatieve pillenautomaat. Deze machine is speciaal ontworpen om ouderen te helpen hun medicatie 
+                        op tijd in te nemen, met een ingebouwde timer en alarm. Wij begrijpen hoe belangrijk het is dat medicatie 
+                        niet wordt vergeten, en met MediTurn streven we ernaar om dit proces eenvoudiger en veiliger te maken.</p>
+                
+                    <p>Onze website biedt uitgebreide informatie over de werking van onze pillenautomaat en hoe 
+                        deze ouderen ondersteunt in hun dagelijkse medicatiebeheer. De automaat zorgt ervoor dat medicatie op het juiste tijdstip wordt afgegeven, zodat gebruikers hun gezondheid beter kunnen beheren.</p>
+                </div>
+            </div>
+        </article>
+
+        <article>
+            <div>
+            <ul>
+                <li><h3>Wat Wij Bieden</h3></li>
+                <li><p><strong>Automatische medicijnafgifte</strong>
                     <br> Onze pillenautomaat geeft automatisch de juiste medicatie af, zodat ouderen zich geen zorgen 
-                    hoeven te maken over het vergeten van hun pillen.
+                    hoeven te maken over het vergeten van hun pillen.</p>
                 </li>
-                <li><strong2>Persoonlijke accounts voor verzorgers en artsen</strong2>
+                <li><p><strong>Persoonlijke accounts voor verzorgers en artsen</strong>
                     <br> Zorgprofessionals kunnen met een persoonlijk account inloggen op onze database om te controleren 
-                    of de oudere de juiste medicatie heeft en deze daadwerkelijk heeft ingenomen.
+                    of de oudere de juiste medicatie heeft en deze daadwerkelijk heeft ingenomen.</p>
                 </li>
             </ul>
-        
-            <h3>Waarom Kiezen voor Ons?</h3>
-            <p class="AS">Bij MediTurn geven we prioriteit aan de veiligheid en het welzijn van onze gebruikers. 
-                Onze pillenautomaat is eenvoudig te gebruiken en zorgt voor een betrouwbare medicatie-inname. 
-                Met onze database kunnen verzorgers en artsen gemakkelijk toezicht houden op de medicatie van hun cliënten,
-                wat bijdraagt aan een betere zorgverlening.</p>
-        
-            <p class="AS">Bedankt voor uw bezoek aan MediTurn! We hopen dat u geniet van het verkennen van onze website 
-                en dat u vindt wat u nodig heeft om het medicatiebeheer voor ouderen te verbeteren.</p>
-        </div>  
+            <img src="" alt="insert image of box">
+            </div>
+        </article>
+
+            <article>
+                <div>            
+                    <img src="" alt="insert image of happy customers">
+                    <div>
+                        <h3>Waarom Kiezen voor Ons?</h3>
+                        <p>Bij MediTurn geven we prioriteit aan de veiligheid en het welzijn van onze gebruikers.<br>
+                            Onze pillenautomaat is eenvoudig te gebruiken en zorgt voor een betrouwbare medicatie-inname. <br>
+                            Met onze database kunnen verzorgers en artsen gemakkelijk toezicht houden op de medicatie van hun cliënten,
+                            wat bijdraagt aan een betere zorgverlening.</p>
+                    
+                        <p>Bedankt voor uw bezoek aan MediTurn!<br>We hopen dat u geniet van het verkennen van onze website 
+                            en dat u vindt wat u nodig heeft om het medicatiebeheer voor ouderen te verbeteren.</p>
+                    </div>
+                </div>
+            </article>  
     </main>  
     <?= footer(); ?>
 </body>
