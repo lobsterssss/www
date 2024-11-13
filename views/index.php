02d3dac04b7fcd6b52d396d051c504618913f232
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-11-13 10:18:11 +0100

    reworked artikal size a bit
    made about page work requires testing for mobile and smaller screen

--- views/index.php
+++ views/index.php
@@ -14,19 +14,17 @@ head();
 
     <main> 
         <header>
-            <div>
                 <h1>Welkom bij MediTurn!</h1>
                 <p>MediTurn biedt slimme medicijnkastjes die ouderen helpen om veilig en tijdig hun medicatie te nemen</p>
-            </div>
+
         </header>
         <article>
-            <div>
+
                 <h1>MediTurn machine</h1>
                 <p>
                     de MediTurn medicijnenkast helpt ouderen om op tijd de goede medicijnen te nemen. Dit doet de kast door middel van een alarm dat afgaat op het moment dat de medicijnen uit de kast komen. het tijdstip dat de medicijnen uit de kast komen wordt ingesteld door de gebruiker.
                     daarnaast kan je op de medicijnenkast aangeven hoe je je voelt door middel van smileys. dit kan je dan later bespreken met je arts.
                 </p>
-            </div>
         </article>
         <article>
                 <h1>Wil je meer weten over ons product? bekijk dan onderstaande video</h1>
